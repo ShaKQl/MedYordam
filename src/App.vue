e18ed64de79227738d36b359cde1b4eb249c03b1
ShaKQl <halimovsvhzod@gmail.com>
2024-08-08 17:06:28 +0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+      <Navbar></Navbar>
+  </div>
+</template>
+
+<script setup>
+  import Navbar from './components/Navbar.vue'
+</script>
+
+<style lang="scss" scoped>
+
+</style>
