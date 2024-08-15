05cfd1177268bb69a2599b7492bc2d08a69571bb
ShaKQl <halimovsvhzod@gmail.com>
2024-08-15 17:10:15 +0500

    Raw html structure for comment section

--- src/components/Main/Main.vue
+++ src/components/Main/Main.vue
@@ -12,14 +12,24 @@
         </button>
       </div>
       <button class="main__search search">
-        <input v-model="searchBar" type="text" class="search__input" placeholder="Поиск направления"/>
+        <input
+          v-model="searchBar"
+          type="text"
+          class="search__input"
+          placeholder="Поиск направления"
+        />
         <img src="../../assets/images/search.svg" alt="" class="search__img" />
       </button>
 
       <div v-for="el in docList" :key="el.id" class="main__specs doc">
         <div class="doc__wrapper">
           <div class="doc__details">
-            <input @click="chngColor(el.id)" type="checkbox" class="doc__checkbox" ref="inputables" />
+            <input
+              @click="chngColor(el.id)"
+              type="checkbox"
+              class="doc__checkbox"
+              ref="inputables"
+            />
             <p class="doc__spcialization">{{ el.name }}</p>
           </div>
           <p ref="comments" :class="`doc__comQuantity `">({{ el.comments }})</p>
@@ -28,13 +38,84 @@
     </div>
 
     <div class="main__recComments recmndtns">
-      <div class="recmndtns__header">
-        <h1 class="recmndtns__headH">Список последних ответов на вопросы <span class="recmndtns__totalCount">(12 493)</span></h1>
+      <div class="recmndtns__header recTitle">
+        <h1 class="recTitle__headbar">
+          Список последних ответов на вопросы
+          <span class="recmndtns__totalCount">(12 493)</span>
+        </h1>
+        <button class="recTitle__sortBtn">
+          <img
+            src="../../assets/images/sort-amount-up.svg"
+            alt=""
+            class="recTitle__svg"
+          />
+          <p class="recTitle__sortby">Сортировать по</p>
+        </button>
+      </div>
+
+      <div class="recmndtns__searchBar searchTerm">
+        <input
+          type="text"
+          class="searchTerm__input"
+          placeholder="Искать по тексту в вопросе"
+        />
+        <button class="searchTerm__searchBtn">
+          <img src="../../assets/images/searchForBtn.svg" alt="" class="searchBtn__svg" />
+          <p class="searchTerm__searchTxt">Поиск</p>
+        </button>
+      </div>
+
+      <div class="recmndtns__commSection recHelp">
+        <div class="recHelp__helpSection comment">
+          <div class="rechelp__header helpData">
+            <p class="helpData__qstionCount">Вопрос: 21408</p>
+            <p class="helpData__qstionDate">Январь 27, 2024 9:41 pm</p>
+          </div>
+          <div class="comment__wrapper">
+            <div class="comment__asker user">
+              <div class="user__qstion question">
+                <div class="question__data">
+                  <p class="question__ansCount">Получено ответов (3)</p>
+                  <p class="question__userData">Lorri Warf (26 лет)</p>
+                </div>
+
+                <div class="question__text">
+                  "Здравствуйте, доктор. В последнее время я заметил(а), что стал(а)
+                  быстро уставать и появилась постоянная усталость, несмотря на
+                  полноценный сон. К тому же, у меня снизился аппетит, и я замечаю
+                  периодические головные боли. Могли бы вы подсказать, что это может быть
+                  и какие обследования мне стоит пройти для уточнения... Читать полностью
+                </div>
+              </div>
+
+              <div class="user__ava avatar">
+                <img src="../../assets/images/Male.png" alt="" class="avatar__img" />
+              </div>
+            </div>
+
+            <div class="comment__answerer answerer">
+              <div class="answerer__ava docAvatar">
+                <img src="../../assets/images/UsmanAkaDoctor.jpg" alt="" class="docAvatar__img" />
+              </div>
+              <div class="user__qstion answer">
+                <div class="answer__data">
+                  <p class="answer__ansDoc">Mary Freund (Гастроинтеролог)</p>
+                  <p class="answer__docDate">Январь 28, 2024 9:41 pm</p>
+                </div>
+
+                <div class="answer__text">
+                  "Здравствуйте, доктор. В последнее время я заметил(а), что стал(а)
+                  быстро уставать и появилась постоянная усталость, несмотря на
+                  полноценный сон. К тому же, у меня снизился аппетит, и я замечаю
+                  периодические головные боли. Могли бы вы подсказать, что это может быть
+                  и какие обследования мне стоит пройти для уточнения... Читать полностью
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-
-
-
   </div>
 </template>
 
@@ -58,7 +139,7 @@ async function fetchDoctors() {
 }
 
 function chngColor(id) {
-  comments.value[id].classList.toggle('doc__comQuantity--active')
+  comments.value[id].classList.toggle("doc__comQuantity--active");
 }
 
 watch(searchBar, () => {
@@ -68,9 +149,9 @@ watch(searchBar, () => {
 });
 
 function cancelChoice() {
-  inputables.value.forEach(elem => elem.checked = false);
-  comments.value.forEach(elem => elem.classList.remove('doc__comQuantity--active'))
-  searchBar.value = ''
+  inputables.value.forEach((elem) => (elem.checked = false));
+  comments.value.forEach((elem) => elem.classList.remove("doc__comQuantity--active"));
+  searchBar.value = "";
 }
 
 onMounted(fetchDoctors);
